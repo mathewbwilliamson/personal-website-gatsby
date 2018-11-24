c1088939031c3d6db7f37ec8f105f2e71cd8f1cc
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-24 17:05:20 -0500

    Finished with making colors replacable through the website

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby'
 import './main.css'
 
 const AboutMe = () => (
-  <div class='aboutMe center grey'>
-    <h1 class='grey'>about me</h1>
-    <p>I'm a <span class='red'>full-stack</span> web engineer.</p>
-    <p>Ever since I was a kid, I <span class='red'>loved</span> technology. Whether it was playing around on my dad's old brick of a laptop, or programming on a state-of-the-art modern machine, it was <span class='red'>always fun</span>. I love figuring things out and <span class='red'>making them work</span>.</p>
-    <p>Recently, I've been <span class='red'>exploring</span> modern web technologies like HTML5, CSS3, JavaScript, React, and Express. Seeing that there are <span class='red'>no limits</span> to what I can build is amazing.</p>
+  <div class='aboutMe center offcolor'>
+    <h1>about me</h1>
+    <p>I'm a <span class='highlight'>full-stack</span> web engineer.</p>
+    <p>Ever since I was a kid, I <span class='highlight'>loved</span> technology. Whether it was playing around on my dad's old brick of a laptop, or programming on a state-of-the-art modern machine, it was <span class='highlight'>always fun</span>. I love figuring things out and <span class='highlight'>making them work</span>.</p>
+    <p>Recently, I've been <span class='highlight'>exploring</span> modern web technologies like HTML5, CSS3, JavaScript, React, and Express. Seeing that there are <span class='highlight'>no limits</span> to what I can build is amazing.</p>
     <p>In addition to programming, I love my two greyhounds, my two cats, and having fun playing boardgames.</p>
   </div>
 )
