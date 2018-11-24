4bcc2cc1d3eb3ebb410e426aaf260bbc1edb928b
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-24 17:00:24 -0500

    Completed the font colors

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,8 +3,8 @@ import { Link } from 'gatsby'
 import './main.css'
 
 const AboutMe = () => (
-  <div class='aboutMe center'>
-    <h1>about me</h1>
+  <div class='aboutMe center grey'>
+    <h1 class='grey'>about me</h1>
     <p>I'm a <span class='red'>full-stack</span> web engineer.</p>
     <p>Ever since I was a kid, I <span class='red'>loved</span> technology. Whether it was playing around on my dad's old brick of a laptop, or programming on a state-of-the-art modern machine, it was <span class='red'>always fun</span>. I love figuring things out and <span class='red'>making them work</span>.</p>
     <p>Recently, I've been <span class='red'>exploring</span> modern web technologies like HTML5, CSS3, JavaScript, React, and Express. Seeing that there are <span class='red'>no limits</span> to what I can build is amazing.</p>
