806f4b4dec08293a79b45acd6739e5006048a28e
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-25 00:17:05 -0500

    Added stars to backgrounds

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => (
     <p>I'm a <span class='highlight'>full-stack</span> web engineer.</p>
     <p>Ever since I was a kid, I <span class='highlight'>loved</span> technology. Whether it was playing around on my dad's old brick of a laptop, or programming on a state-of-the-art modern machine, it was <span class='highlight'>always fun</span>. I love figuring things out and <span class='highlight'>making them work</span>.</p>
     <p>Recently, I've been <span class='highlight'>exploring</span> modern web technologies like HTML5, CSS3, JavaScript, React, and Express. Seeing that there are <span class='highlight'>no limits</span> to what I can build is amazing.</p>
-    <p>In addition to programming, I love my two greyhounds, my two cats, and having fun playing boardgames.</p>
+    <p>In addition to programming, I love my wife, my two greyhounds, my two cats, and having fun playing boardgames.</p>
   </div>
 )
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import ProjectItem from './ProjectItem'
 const Projects = () => (
   
   <div class="projects center">
+    <div class='paralax-background'></div>
     <h1>Recent Projects</h1>
     <ul>
       <ProjectItem />
