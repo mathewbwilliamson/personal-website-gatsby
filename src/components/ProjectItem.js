86808d6c511aabadb9de326eaacd8622e78108c2
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-25 12:05:04 -0500

    Fixed some minor styling bugs and started working on project items

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,14 +4,21 @@ import './projectitem.css'
 
 const ProjectItem = (projObject) => (
   
-  <div class="left">
-    <li class="post-block">
-      <div class="right photo">
-        <img src="content/03-projects/01-dresses-and-beer/1.thumb.png" />
-      </div>
-      <h1 class="post-title"><a href="/projects/dresses-and-beer">This Portfolio</a></h1>
-      <p class="post-meta">Branding &amp; Website</p>
-    </li>
+  <div class='card'>
+    <div class='cardTitle'>
+      <h2>Title Goes Here</h2>
+    </div>
+    <div class='cardImage'>
+      <img src='' />
+    </div>
+
+    <p class='cardNotes'>This web-based app is designed to track leads for a service-oriented business. When a lead is entered, the app will automatically add upcoming "events" to the lead. These events will be automated emails, automated text messages, and reminders to call the lead. The emails will be eventually handled by Send Grid while text messages will be handled by Twillio.</p>
+    <p class='cardBuiltIn'>Built in October 2018 with HTML, CSS, CSS Grid, CSS Flexbox, React, Redux, Node.js, Express, MongoDB</p>
+    <a href='' class='cardTryIt'>Try It Out</a>
+    <p class='cardUsername'>Username</p>
+    <p class='cardPassword'>Password</p>
+    <a href='' class='cardGithub'>Look at the code on Github</a>
+      
   </div>
   
 )
