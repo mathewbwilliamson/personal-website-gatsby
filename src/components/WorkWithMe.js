c66b8e5f779e286d177e11d8f430d3330c6411aa
Matt Williamson <mathewbwilliamson@gmail.com>
2018-12-02 01:01:53 -0500

    Added icons for the work with me section

--- src/components/WorkWithMe.js
+++ src/components/WorkWithMe.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 import './main.css'
 import Img from 'gatsby-image'
+import { FaGithubSquare, FaFilePdf, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+
 
 const WorkWithMe = ({data}) => {
   console.log(data);
@@ -11,15 +14,47 @@ const WorkWithMe = ({data}) => {
     <p><a href="./documents/Williamson - Web Developer Resume v2.pdf" target='_blank'>Download or view my resume.</a></p>
     <p>Send me an email, connect with me on LinkedIn or view my Github.</p>
     <section class='icon_container'>
+
       <a href="https://www.linkedin.com/in/matt-b-williamson/" target='_blank'>
         <section class='icon'>
-          <img height="32" width="32" src="./images/linkedin.svg" />
-          {/* <Img fluid={data.linkedin.childImageSharp.fluid} /> */}
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <div>
+              <FaLinkedin />
+            </div>
+          </IconContext.Provider>
         </section>
       </a>
-      <a href="https://github.com/mathewbwilliamson" target='_blank'><section class='icon'><img height="32" width="32" src="./images/github-circle.svg" /></section></a>
-      <a href="mailto:mathewbwilliamson@gmail.com"><section class='icon'><img height="32" width="32" src="./images/email.svg" /></section></a>
-      <a href="./documents/Williamson - Web Developer Resume v2.pdf" target='_blank'><section class='icon'><img height="32" width="32" src="./images/file-pdf-box.svg" /></section></a>
+
+      <a href="https://github.com/mathewbwilliamson" target='_blank'>
+        <section class='icon'>
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <div>
+              <FaGithubSquare />
+            </div>
+          </IconContext.Provider>
+        </section>
+      </a>
+
+      <a href="mailto:mathewbwilliamson@gmail.com">
+        <section class='icon'>
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <div>
+              <FaEnvelope />
+            </div>
+          </IconContext.Provider>
+        </section>
+      </a>
+
+      <a href="./documents/Williamson - Web Developer Resume v2.pdf" target='_blank'>
+        <section class='icon'>
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <div>
+              <FaFilePdf />
+            </div>
+          </IconContext.Provider>
+        </section>
+      </a>
+
     </section>
   </div>)
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
 import Navbar from './Navbar'
 // import './layout.css'
 import './main.css'
